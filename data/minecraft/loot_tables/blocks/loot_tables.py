37f746c9036b7ce3d27fd80cde297a68f3fa27c9
Compsogbrickus <compsogbrickus@gmail.com>
2022-08-17 17:34:39 -0400

    Completed MVP for Mapwreck 5.

--- data/minecraft/loot_tables/blocks/loot_tables.py
+++ data/minecraft/loot_tables/blocks/loot_tables.py
@@ -12,13 +12,48 @@ dir = script.parent.absolute()
 with open(path.join(dir, name + ".csv"), newline="") as csv:
     csv_reader = reader(csv)
     header = next(csv_reader)
+    # Header has Type, Base Block, Block, Count, Block, Count
+
     for row in csv_reader:
-        if row[1] == "":
-            structure = loot_table_utils.blocks_empty()
+        type = row[0]
+        base_block = row[1]
+        block_1 = row[2]
+        count_1 = row[3]
+        block_2 = row[4]
+        count_2 = row[5]
+
+        if (type == "custom") or (type == "review"):
+            continue
+        elif type == "void":
+            structure = loot_table_utils.blocks_void()
+        elif type == "normal_0":
+            structure = loot_table_utils.blocks_normal_0(base_block, int(count_1))
+        elif type == "normal_1":
+            structure = loot_table_utils.blocks_normal_1(block_1, int(count_1))
+        elif type == "normal_2":
+            structure = loot_table_utils.blocks_normal_2(block_1, int(count_1), block_2, int(count_2))
+        elif type == "silk_0":
+            structure = loot_table_utils.blocks_silk_0(base_block, int(count_1))
+        elif type == "silk_1":
+            structure = loot_table_utils.blocks_silk_1(base_block, block_1, int(count_1))
+        elif type == "silk_2":
+            structure = loot_table_utils.blocks_silk_2(base_block, block_1, int(count_1), block_2, int(count_2))
+        elif type == "shears_0":
+            structure = loot_table_utils.blocks_shears_0(base_block, int(count_1))
+        elif type == "shears_1":
+            structure = loot_table_utils.blocks_shears_1(base_block, block_1, int(count_1))
+        elif type == "shears_2":
+            structure = loot_table_utils.blocks_shears_2(base_block, block_1, int(count_1), block_2, int(count_2))
+        elif type == "slab_0":
+            structure = loot_table_utils.blocks_slab_0(base_block)
+        elif type == "slab_1":
+            structure = loot_table_utils.blocks_slab_1(base_block, block_1)
+        elif type == "slab_2":
+            structure = loot_table_utils.blocks_slab_2(base_block, block_1, block_2)
         else:
-            structure = loot_table_utils.blocks(row[0], row[1], int(row[2]))
+            print("Skipping block loot table with base block " + base_block + ".")
 
         json_structure = json.dumps(structure, indent=4)
 
-        with open(path.join(dir, row[0] + ".json"), "w") as file_out:
+        with open(path.join(dir, base_block + ".json"), "w") as file_out:
             file_out.write(json_structure)

4664454b075f5d55a88148875b06905a16d992bc
Compsogbrickus <compsogbrickus@gmail.com>
2023-01-06 18:54:33 -0500

    unchecked ltos support

--- scripts/loot_tables/ltos.py
+++ scripts/loot_tables/ltos.py
@@ -12,11 +12,13 @@ simple_blocks_dir = script.parents[2].absolute()
 block_loot_tables_dir = path.join(simple_blocks_dir, "data/minecraft/loot_tables/blocks")
 
 for loot_table in Path(block_loot_tables_dir).glob("*.json"):
-    with open(loot_table, "+") as loaded_loot_table:
+    with open(loot_table, "r+") as loaded_loot_table:
         current_loot_table = json.load(loaded_loot_table)
         
         base_block = path.splitext(path.basename(loot_table))[0]
         current_loot_table["pools"].append(loot_table_utils.ltos(base_block))
+
+        loaded_loot_table.seek(0)
 
         json_structure = json.dumps(current_loot_table, indent=4)
         loaded_loot_table.write(json_structure)

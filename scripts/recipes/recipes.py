9a9bc143a8ee0115414d427661581a1854cebdad
Compsogbrickus <compsogbrickus@gmail.com>
2023-07-18 12:20:29 -0400

    Backport updates from Mason

--- scripts/recipes/recipes.py
+++ scripts/recipes/recipes.py
@@ -21,7 +21,8 @@ functions_dir = path.join(simple_blocks_dir, "data/simple_blocks/functions")
 give_recipes_dir = path.join(functions_dir, "give_recipes")
 take_recipes_dir = path.join(functions_dir, "take_recipes")
 
-recipe_types = ["crafting_shaped",
+recipe_types = ["crafting_shaped_small",
+                "crafting_shaped_large",
                 "crafting_shapeless_small",
                 "crafting_shapeless_large",
                 "stonecutting",
@@ -55,51 +56,80 @@ with ExitStack() as stack:
         output_count = 0 if output_count == "" else int(output_count)
 
         if station == "crafting_shaped":
-            id = 0
             recipe_structure = recipe_utils.crafting_shaped(output, int(output_count), inputs[:9])
+            
+            pattern_rows = recipe_structure.get("pattern", "")
+            height = len(pattern_rows)
+            width = len(pattern_rows[0])
+
+            if (height < 3) and (width < 3):
+                id = 0
+            else:
+                id = 1
+
             filename = output
         elif station == "crafting_shapeless":
             inputs_length, recipe_structure = recipe_utils.crafting_shapeless(output, int(output_count), inputs[:9])
-            if inputs_length > 4:
+            
+            if inputs_length < 5:
                 id = 2
             else:
-                id = 1
+                id = 3
+            
             filename = output
         elif station == "stonecutting":
-            id = 3
             recipe_structure = recipe_utils.stonecutting(output, int(output_count), inputs[0])
+
+            id = 4
+
             filename = output + "_from_" + inputs[0] + "_stonecutting"
         elif station == "smithing":
-            id = 4
             recipe_structure = recipe_utils.smithing(output, output_count, inputs[:2])
+
+            id = 5
+
             filename = output + "_smithing"
         elif station == "campfire_cooking":
-            id = 5
             recipe_structure = recipe_utils.cooking(station, experience, cooking_time, output, inputs[0])
+
+            id = 6
+
             filename = output + "_from_campfire_cooking"
         elif station == "smoking":
-            id = 6
             recipe_structure = recipe_utils.cooking(station, experience, cooking_time, output, inputs[0])
+
+            id = 7
+
             filename = output + "_from_smoking"
         elif station == "blasting":
-            id = 7
             recipe_structure = recipe_utils.cooking(station, experience, cooking_time, output, inputs[0])
+
+            id = 8
+
             filename = output + "_from_blasting"
         elif station == "blasting_clear":
-            id = 7
             recipe_structure = recipe_utils.cooking("blasting", experience, cooking_time, output, inputs[0])
+
+            id = 8
+
             filename = output + "_from_blasting_" + inputs[0]
         elif station == "smelting":
-            id = 8
             recipe_structure = recipe_utils.cooking(station, experience, cooking_time, output, inputs[0])
+
+            id = 9
+
             filename = output + "_from_smelting"
         elif station == "smelting_clear":
-            id = 8
             recipe_structure = recipe_utils.cooking("smelting", experience, cooking_time, output, inputs[0])
+
+            id = 9
+
             filename = output + "_from_smelting_" + inputs[0]
         else:
-            id = -1
             recipe_structure = None
+
+            id = -1
+
             print("Adding recipe with no type " + name)
 
         if name != "":
